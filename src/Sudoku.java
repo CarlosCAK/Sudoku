1e5e61b5ecc991a5db638074d044bd5f1cc56ac5
CarlosCAK <carloskviatkowsky@gmail.com>
2023-11-07 20:49:27 -0300

    Update Sudoku.java

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -19,7 +19,8 @@ public class Sudoku {
 				somaColuna7 = 0, somaColuna8 = 0, somaColuna9 = 0;
 		int somaJogo = 0;
 		boolean certo = false;
-
+		int cont1 = 0, cont2 = 0, cont3 = 0, cont4 = 0, cont5 = 0, cont6 = 0, cont7 = 0, cont8 = 0, cont9 = 0;
+		
 		do {
 			System.out.println("Digite a quantidade de jogadores que irao jogar Sudoku");
 			quantidade = sc.nextInt();
@@ -39,6 +40,7 @@ public class Sudoku {
 			somaColuna1 = 0; somaColuna2 = 0; somaColuna3 = 0; somaColuna4 = 0;somaColuna5 = 0;somaColuna6 = 0;somaColuna7 = 0;somaColuna8 = 0;somaColuna9 = 0;
 			somaLinha1 = 0; somaLinha2 = 0; somaLinha3 = 0; somaLinha4 = 0; somaLinha5 = 0; somaLinha6 = 0; somaLinha7 = 0; somaLinha8 = 0; somaLinha9 = 0;
 			somaJogo = 0;
+			cont1 = 0; 	cont2 = 0; 	cont3 = 0; 	cont4 = 0;	cont5 = 0;	cont6 = 0;	cont7 = 0; 	cont8 = 0;	cont9 = 0; 
 			
 			for (int linha = 0; linha < 9; linha++) {
 				for (int coluna = 0; coluna < 9; coluna++) {
@@ -50,15 +52,38 @@ public class Sudoku {
 			for (int linha = 0; linha < 9; linha++) {
 				for (int coluna = 0; coluna < 9; coluna++) {
 					tabuleiro[linha][coluna] = sc.nextInt();
+					
+					if(tabuleiro[linha][coluna] == 1 ) {
+						cont1++;
+					}
+					if(tabuleiro[linha][coluna] == 2 ) {
+						cont2++;
+					}
+					if(tabuleiro[linha][coluna] == 3 ) {
+						cont3++;
+					}
+					if(tabuleiro[linha][coluna] == 4 ) {
+						cont4++;
+					}
+					if(tabuleiro[linha][coluna] == 5 ) {
+						cont5++;
+					}
+					if(tabuleiro[linha][coluna] == 6 ) {
+						cont6++;
+					}
+					if(tabuleiro[linha][coluna] == 7 ) {
+						cont7++;
+					}
+					if(tabuleiro[linha][coluna] == 8 ) {
+						cont8++;
+					}
+					if(tabuleiro[linha][coluna] == 9 ) {
+						cont9++;
+					}
 
 					if (tabuleiro[linha][coluna] < 0 || tabuleiro[linha][coluna] > 9) {
 						coluna--;
-					}
-					for (int linha1 = 0; linha1 < 9; linha1++) {
-						for (int coluna1 = 0; coluna1 < 9; coluna1++) {
-							System.out.print("[" + tabuleiro[linha1][coluna1] + "] ");
-						}
-						System.out.println("\n");
+						System.out.println("incorreto, repita com o valor adequado");
 					}
 
 				}
@@ -124,10 +149,12 @@ public class Sudoku {
 				}
 			}
 			if(somaColuna1 == 45 && somaColuna2 == 45 && somaColuna3 == 45 && somaColuna4 == 45 && somaColuna5 == 45 && somaColuna6 == 45 && somaColuna7 == 45 && somaColuna8 == 45 && somaColuna9 == 45 && somaLinha1 == 45 &&
-					somaLinha2 == 45 && somaLinha3 == 45 && somaLinha4 == 45 && somaLinha5 == 45 && somaLinha6 == 45 && somaLinha7 == 45 && somaLinha8 == 45 && somaLinha9 == 45 && somaJogo == 405)  {
+					somaLinha2 == 45 && somaLinha3 == 45 && somaLinha4 == 45 && somaLinha5 == 45 && somaLinha6 == 45 && somaLinha7 == 45 && somaLinha8 == 45 && somaLinha9 == 45 && somaJogo == 405 && 
+					cont1 == 9 && cont2 == 9 && cont3 == 9 && cont4 == 9 && cont5 == 9 && cont6 == 9 &&cont7 == 9 && cont8 == 9 && cont9 == 9)  {
 				certo = true;
 			}
 
+			
 	
 
 			if( certo == true) {
@@ -136,9 +163,10 @@ public class Sudoku {
 			else {
 				System.out.println("Errada");
 			}
-		}
+			
 	
 	}
 
 	}
+}
 
